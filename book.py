bd2eeba117ba74b4e99de9b7cde937fc3ebc59df
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-15 14:17:07 -0400

    created classmethod create

--- book.py
+++ book.py
@@ -5,25 +5,32 @@ class Book:
     on_shelf = []
     on_loan = []
 
-    def __init__(self, title, author, isbn:
+    def __init__(self, title, author, isbn):
         self.title = title
         self.author = author
         self.isbn = isbn
     
-    def borrow(self):
+    # def borrow(self):
+    
+    # def return_to_library(self):
+    
+    # def lent_out(self):
 
-    def return_to_library(self):
-    
-    def lent_out(self):
+    @classmethod
+    def create(cls, title, author, isbn):
+        new_book = Book(title, author, isbn)
+        cls.on_shelf.append(new_book)
+        return new_book
 
-    @classmethod
-    def create(self):
     
-    @classmethod
-    def current_due_date(self):
+    # @classmethod
+    # def current_due_date(cls):
     
-    @classmethod
-    def overdue_books(self):
+    # @classmethod
+    # def overdue_books(cls):
     
-    @classmethod
-    def browse(self):
+    # @classmethod
+    # def browse(cls):
+
+harry = Book.create("Harry", "John", 1234)
+print(Book.on_shelf)
