dae422fe8c613cc12480ec6c0046d7e124460a63
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-15 14:03:21 -0400

    made book class

---
+++ book.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+class Book:
+
+    on_shelf = []
+    on_loan = []
+
+    def __init__(self, title, author, isbn:
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+    
+    def borrow(self):
+
+    def return_to_library(self):
+    
+    def lent_out(self):
+
+    @classmethod
+    def create(self):
+    
+    @classmethod
+    def current_due_date(self):
+    
+    @classmethod
+    def overdue_books(self):
+    
+    @classmethod
+    def browse(self):
