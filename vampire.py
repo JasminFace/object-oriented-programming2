1b3b7f3a90db20df47bd0f632cce36221749a102
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-15 16:37:50 -0400

    created classmethod create

--- vampire.py
+++ vampire.py
@@ -7,3 +7,9 @@ class Vampire:
         self.age = age
         self.coffin = in_coffin
         self.drank = drank_blood_today
+    
+    @classmethod
+    def create(cls, name, age, in_coffin = True, drank_blood_today = False):
+        new_vampire = Vampire(name, age, in_coffin, drank_blood_today)
+        cls.coven.append(new_vampire)
+        return new_vampire
